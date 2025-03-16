7c0e547c11955f0f086e07b5e267fe1157aa075e
Manojkumar143341 <manoj166174@gmail.com>
2025-03-16 19:04:40 +0530

    Resolved merge conflicts

--- Backend/routes/grievanceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { submitGrievance, getAllGrievances } = require("../controllers/grievanceController");
-
-const router = express.Router();
-
-router.post("/", submitGrievance);
-router.get("/", getAllGrievances);
-
-module.exports = router;
